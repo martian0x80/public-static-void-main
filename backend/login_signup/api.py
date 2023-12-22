35f142dc44bcef2326c731dbdfdf57816530cd8d
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2023-12-22 22:56:06 +0530

    add status key with serializer validation error response

--- backend/login_signup/api.py
+++ backend/login_signup/api.py
@@ -1,18 +1,24 @@
 from rest_framework import generics, permissions, mixins, authentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from .serializer import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
 
 class RegisterApi(APIView):
     def post(self, request, *args,  **kwargs):
         serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({
-            "user": UserSerializer(user).data,
-            "message": "User Created Successfully. Login to grab JWT token.",
-        })
+        if serializer.is_valid(raise_exception=False):
+            user = serializer.save()
+            return Response({
+                "user": UserSerializer(user).data,
+                "message": "User Created Successfully. Login to grab JWT token.",
+                "status": "success"
+            })
+        else:
+            return Response(
+                serializer.errors | {'status': 'Failed'}, status=HTTP_400_BAD_REQUEST
+            )
 class DeleteApi(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAdminUser]
--- backend/login_signup/serializer.py
+++ backend/login_signup/serializer.py
@@ -10,14 +10,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email')
         extra_kwargs = {
-            'password':
-                {'write_only': True},
+            'password': {'write_only': True},
+            'first_name': {'required': True, 'allow_blank': False},
+            'last_name': {'required': True, 'allow_blank': False},
+            'email': {'required': True, 'allow_blank': False},
         }
+
     def create(self, validated_data):
         user = User.objects.create_user(username=validated_data['username'], first_name=validated_data['first_name'], last_name=validated_data['last_name'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
         return user
+    # object wide validation
+    # Doesn't work idk why
+    # def validate(self, data):
+    #     for field, value in data.items():
+    #         if len(value) == 0:
+    #             raise serializers.ValidationError({
+    #                 'status': 'Failed',
+    #                 'details': f'The field "{field}" is required and cannot be blank.'
+    #             })
+    #     return data
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
